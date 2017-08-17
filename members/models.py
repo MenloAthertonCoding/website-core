ac41c9965a69036ac41db3dbf7aa6641594f4870
Daniel Montgomery <d.montgrmry@gmail.com>
2017-08-16 20:59:42 -0700

    Update member model and makemigrations

--- members/models.py
+++ members/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Member(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    profile_image = models.ImageField(upload_to='base/profiles/') # Requires Pillow (PIL fork)
-    website = models.SlugField(blank=True) # not required
+    email = models.EmailField(verbose_name='Preferred email')
+    profile_image = models.ImageField(upload_to='members/profiles/') # Requires Pillow (PIL fork)
+    website = models.CharField(max_length=200, verbose_name='Website URL', blank=True) # not required
 
     @property
     def full_name(self):
@@ -16,3 +17,4 @@ class Member(models.Model):
 
     class Meta:
         unique_together = (('first_name', 'last_name'),)
+        ordering = ('first_name',)
