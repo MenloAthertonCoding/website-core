9702c426a388cfbb6d0f57755d1aa1cef3001b3d
Daniel Montgomery <d.montgrmry@gmail.com>
2017-08-16 18:00:14 -0700

    Set MEDIA_ROOT and MEDIA_URL in project settings

--- websitecore/settings.py
+++ websitecore/settings.py
@@ -127,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media files
+# https://docs.djangoproject.com/en/1.11/ref/settings/#std:setting-MEDIA_ROOT
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
