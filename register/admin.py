939480d267357f68252c5f1b767388582903d031
Daniel Montgomery <d.montgrmry@gmail.com>
2017-08-19 23:31:35 -0700

    Add TeamAdmin and ContestantAdmin model admin

--- register/admin.py
+++ register/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Team, Contestant
+
+@admin.register(Contestant)
+class ContestantAdmin(admin.ModelAdmin):
+    search_fields = ['first_name', 'last_name', 'email', 'team']
+
+    list_display = ('first_name', 'last_name', 'email', 'team')
+    list_display_links = ('first_name', 'last_name', 'email')
+
+    readonly_fields = ('team',)
+
+    fieldsets = [
+        ('Name', {'fields': ['first_name', 'last_name']}),
+        ('Personal Information', {'fields': ['email', 'team']}),
+    ]
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'school']
+
+    class ContestantInline(admin.TabularInline):
+        model = Contestant
+        extra = 2
+
+    list_display = ('name', 'school')
+    list_display_links = ('name',)
+
+    fieldsets = [
+        ('Information', {'fields': ['name', 'school']}),
+    ]
+
+    inlines = [ContestantInline]
