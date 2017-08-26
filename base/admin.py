dc2e39663f20304dc75184e7258eca99619314f0
Daniel Montgomery <d.montgrmry@gmail.com>
2017-08-25 19:11:58 -0700

    Add BaseAdminSite

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+from django.contrib.admin import AdminSite, site
 
-# Register your models here.
+class BaseAdminSite(AdminSite):
+    site_header = 'Menlo Atherton Coding'
+    site_title = 'Menlo Atherton Coding'
+
+    def __init__(self, *args, **kwargs):
+        super(BaseAdminSite, self).__init__(*args, **kwargs)
+        self._registry.update(site._registry)
+
+admin_site = BaseAdminSite(name='MAC Admin')
